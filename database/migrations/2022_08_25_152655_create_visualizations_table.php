8e89d4224e40e68a53d8cc1a89ec188ecf4ab1a3
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-08-25 17:32:59 +0200

    fine creazione migration di base

---
+++ database/migrations/2022_08_25_152655_create_visualizations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisualizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visualizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visualizations');
+    }
+}
