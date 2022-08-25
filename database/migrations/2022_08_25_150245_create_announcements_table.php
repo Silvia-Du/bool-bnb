d27bc86176428c00b6021a12cf0775c48ac2666d
Silvia-Du <silviadu@hotmail.it>
2022-08-25 17:15:05 +0200

    Primo commit: impostazione base di progetto e base migration

---
+++ database/migrations/2022_08_25_150245_create_announcements_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->integer('rooms');
+            $table->integer('beds');
+            $table->integer('bathrooms');
+            $table->integer('mq');
+            $table->string('adress');
+            $table->string('city');
+            $table->string('country');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->string('img')->nullable();
+            $table->string('img_original_name')->nullable();
+            $table->boolean('is_visible')->default(1);
+            $table->string('house_type');
+            $table->string('room_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
