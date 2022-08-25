baa051a0dc63aa38d27138344b24f443d163a6ec
GioCri01 <giovanni.crispino94@icloud.com>
2022-08-25 18:37:29 +0200

    creato seeder per la tabella services

---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Service;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $services = ["bagno","lavatrice","Wi-Fi","Tv","Aria Condizionata","Macchina del caffè","piscina","biancheria da letto","minibar"];
+
+        foreach($services as $service)
+        {
+
+         $new_service = new Service();
+         $new_service->name = $service;
+         $new_service->save();
+
+
+        }
+    }
+}
