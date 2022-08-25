892b0271007ae48bee24d5c006b3b77844667ead
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-08-25 18:17:10 +0200

    completato il seeder AnnouncementsTableSeeder

--- database/seeds/AnnouncementsTableSeeder.php
+++ database/seeds/AnnouncementsTableSeeder.php
@@ -14,10 +14,22 @@ class AnnouncementsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
        for($i=0; $i<10; $i++){
-
         $new_announcement = new Announcement();
         $new_announcement->title = $faker->words(4);
         $new_announcement->slug = Announcement::slugGenerator($new_announcement->title);
+        $new_announcement->rooms = $faker->numberBetween(1, 10);
+        $new_announcement->beds = $faker->numberBetween(1, 5);
+        $new_announcement->bathrooms = $faker->numberBetween(1, 5);
+        $new_announcement->mq = $faker->numberBetween(50, 250);
+        $new_announcement->address = $faker->streetAddress();
+        $new_announcement->city = $faker->city();
+        $new_announcement->city = $faker->country();
+        $new_announcement->latitude = $faker->latitude($min = -90, $max = 90);
+        $new_announcement->longitude = $faker->longitude($min = -180, $max = 180);
+        $new_announcement->visible = $faker->numberBetween(0, 1);
+        $new_announcement->house_type = 'Villa';
+        $new_announcement->room_type = 'Stanza singola';
+        $new_announcement->save();
        }
     }
 }
