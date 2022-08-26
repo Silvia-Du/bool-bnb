b8b6ed58e109d9f624be610553e21ec69f4e7b84
IvanRusso91 <iv.russo91@gmail.com>
2022-08-26 12:07:55 +0200

    update tabella messaggi

---
+++ database/migrations/2022_08_26_095602_update_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+
+           $table->unsignedBigInteger('announcement_id')
+                ->after('id');
+
+            $table->foreign('announcement_id')
+                ->references('id')
+                ->on('announcement')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+
+            $table->dropForeign(['announcement_id']);
+            $table->dropColumn('announcement_id');
+        });
+    }
+}
