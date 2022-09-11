cf174c1736d7bde7fe560a693876874dce7149ad
GioCri01 <giovanni.crispino94@icloud.com>
2022-09-11 17:46:13 +0200

    fix-host

--- resources/js/components/pages/BecomeHostComp.vue
+++ resources/js/components/pages/BecomeHostComp.vue
@@ -68,7 +68,7 @@
  <div class="container-fluid bg-light mt-5 ">
      <div class="row">
          <div class="col-12 col-md-12 col-lg-6 p-0 m-0">
-              <!-- <img class="d-block w-100" src="../../../../public/images/ezgif.com-gif-maker (2).gif" alt="Third slide"> -->
+            <img class="d-block w-100" src="../../../../public/images/ezgif.com-gif-maker (2).gif" alt="Third slide">
          </div>
          <div class="col-12 col-md-12 col-lg-6 bg-dark p-0 m-0 d-flex justify-content-center align-items-center">
              <div class="d-flex justify-content-center">
